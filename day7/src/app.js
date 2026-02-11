3313c772049d27bbc8038f719f522253e65c088e
manaskg <manaskumarghosh91@gmail.com>
2026-02-12 03:32:41 +0530

    date7 done

--- day7/src/app.js
+++ day7/src/app.js
@@ -18,4 +18,12 @@ app.post("/notes", async (req, res) => {
   });
 });
 
+app.get("/notes", async (req, res) => {
+  const notes = await noteModel.find();
+  res.status(200).json({
+    message: "notes fetched successfully",
+    notes,
+  });
+});
+
 module.exports = app;
