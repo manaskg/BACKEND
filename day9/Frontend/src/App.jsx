513a98f1ac6ab8de11f8efa3dd8e96cc05e4f49e
manaskg <manaskumarghosh91@gmail.com>
2026-02-19 02:50:10 +0530

    feat: implement note fetching, creation, and deletion functionality in App component

--- day9/Frontend/src/App.jsx
+++ day9/Frontend/src/App.jsx
@@ -1,39 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
 
 const App = () => {
-  const [notes, setNotes] = useState([
-    // {
-    //   title: "title 1",
-    //   description: "description 1",
-    // },
-    // {
-    //   title: "title 2",
-    //   description: "description 2",
-    // },
-    // {
-    //   title: "title 3",
-    //   description: "description 3",
-    // },
-    // {
-    //   title: "title 4",
-    //   description: "description 4",
-    // },
-  ]);
+  const [notes, setNotes] = useState([]);
 
-  axios.get("http://localhost:3000/api/notes").then((res) => {
-    setNotes(res.data.notes);
-  });
+  function fetchNotes() {
+    axios.get("http://localhost:3000/api/notes").then((res) => {
+      setNotes(res.data.notes);
+    });
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    const { title, description } = e.target.elements;
+    console.log(title.value, description.value);
+
+    const newNote = { title: title.value, description: description.value };
+
+    axios.post("http://localhost:3000/api/notes", newNote).then((res) => {
+      console.log(res.data);
+      fetchNotes();
+    });
+  }
+
+  function handleDeleteNote(noteId) {
+    axios.delete("http://localhost:3000/api/notes/" + noteId).then((res) => {
+      console.log(res.data);
+      fetchNotes();
+    });
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
 
   return (
     <div>
+      <form className="create-note-form" onSubmit={submitHandler}>
+        <input className="title" name="title" type="text" placeholder="title" />
+        <input
+          className="description"
+          name="description"
+          type="text"
+          placeholder="description"
+        />
+        <button className="submit" type="submit">
+          Create
+        </button>
+      </form>
+
       <div className="notes">
         {notes.map((note) => {
           return (
-            <div className="note">
+            <div className="note" key={note._id}>
               <h2>{note.title}</h2>
               <p>{note.description}</p>
+              <button
+                onClick={() => {
+                  handleDeleteNote(note._id);
+                }}
+              >
+                delete
+              </button>
             </div>
           );
         })}
