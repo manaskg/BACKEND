213186de71ea07985c393738de0c1a03fb4c842f
manaskg <manaskumarghosh91@gmail.com>
2026-03-05 03:40:42 +0530

    feat: initialize day12 project with Express, MongoDB, and JWT authentication
    - Added package.json with necessary dependencies
    - Created server.js to set up the Express server and connect to the database
    - Implemented app.js to configure middleware and routes
    - Established database connection in database.js using Mongoose
    - Defined user model in user.model.js with schema for user data
    - Created auth.routes.js for user registration with JWT token generation and cookie handling

---
+++ day12/src/routes/auth.routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user.model.js");
+
+const authRouter = express.Router();
+
+authRouter.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const isUserExists = await userModel.findOne({ email });
+
+  if (isUserExists) {
+    return res.status(409).json({
+      message: "user already exists with this email",
+    });
+  }
+
+  const user = await userModel.create({
+    email,
+    password,
+    name,
+  });
+
+  const token = jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("jwt_token", token);
+
+  res.status(201).json({
+    message: "user created successfully",
+    user,
+    token,
+  });
+});
+
+module.exports = authRouter;
