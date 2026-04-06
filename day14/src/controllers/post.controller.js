e82f2c3eae857e7093335c3ad252001ac6dae7c0
manaskg <manaskumarghosh91@gmail.com>
2026-04-06 14:35:25 +0530

    feat: enhance createPostController with JWT authentication and structured response

--- day14/src/controllers/post.controller.js
+++ day14/src/controllers/post.controller.js
@@ -1,21 +1,51 @@
 const postModel = require("../models/post.model.js");
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
 
 const imagekit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
 
 async function createPostController(req, res) {
-    // console.log(req.body);
-    // console.log(req.file);
+  console.log(req.body);
+  console.log(req.file);
+
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "user is not authorized",
+    });
+  }
+
+  let decoded = null;
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid token, Unauthorized user",
+    });
+  }
+
+  console.log(decoded);
 
   const file = await imagekit.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "Test",
+    folder: "cohor-2-insta-posts",
   });
 
-  res.send(file);
+  const post = await postModel.create({
+    caption: req.body.caption,
+    imgUrl: file.url,
+    user: decoded.id,
+  });
+
+  res.status(201).json({
+    message: "post created successfully",
+  });
 }
 
 module.exports = { createPostController };
