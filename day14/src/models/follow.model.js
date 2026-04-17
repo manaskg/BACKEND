3e5020871fc9f4d2b8ddb27b957a8f6ef7548855
manaskg <manaskumarghosh91@gmail.com>
2026-04-17 13:22:58 +0530

    feat: add follow model with follower and following schema

---
+++ day14/src/models/follow.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const followSchema = new mongoose.Schema(
+  {
+    follower: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: [true, "Follower is required"],
+    },
+    following: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      requirq: [true, "Following is required"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const followModel = mongoose.model("follows", followSchema);
+
+module.exports = followModel;
