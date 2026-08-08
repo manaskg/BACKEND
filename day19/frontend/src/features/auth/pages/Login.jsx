b23bf1891e505ac67e7ad6ba096cee978da627d9
manaskg <manaskumarghosh91@gmail.com>
2026-08-09 01:49:13 +0530

    feat: add authentication functionality with login and registration forms, including axios integration and CORS support

--- day19/frontend/src/features/auth/pages/Login.jsx
+++ day19/frontend/src/features/auth/pages/Login.jsx
@@ -1,19 +1,62 @@
-import React from 'react'
-import '../style/form.scss'
+import "../style/form.scss";
+import { Link } from "react-router";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    axios
+      .post(
+        "http://localhost:3000/api/auth/login",
+        {
+          username,
+          password,
+        },
+        { withCredentials: true },
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
+
   return (
     <main>
       <div className="form-container">
         <h1>Login</h1>
-        <form>
-          <input type="text" name="username" placeholder='Enter username' />
-          <input type="text" name='password' placeholder='Enter password' />
+        <form onSubmit={handleSubmit}>
+          <input
+            onInput={(e) => {
+              setUsername(e.target.value);
+            }}
+            type="text"
+            name="username"
+            placeholder="Enter username"
+          />
+          <input
+            onInput={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="text"
+            name="password"
+            placeholder="Enter password"
+          />
           <button>Submit</button>
         </form>
+
+        <p>
+          Don't have an account?{" "}
+          <Link className="toggleAuthForm" to="/register">
+            Register
+          </Link>
+        </p>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
