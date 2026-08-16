dc252aa444a5cf526c0cb7f64edcc1293ade168a
manaskg <manaskumarghosh91@gmail.com>
2026-08-16 23:08:18 +0530

    feat: implement registration form with state management and loading handling

--- day19/frontend/src/features/auth/pages/Register.jsx
+++ day19/frontend/src/features/auth/pages/Register.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import { useState } from "react";
 import "../style/form.scss";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useAuth } from "../hooks/useAuth.js";
 
 const Register = () => {
-  const handleSubmit = (e) => {
+  const { user, loading, handleRegister } = useAuth();
+
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    await handleRegister(email, username, password);
+    console.log("user registered");
+    navigate("/");
   };
+
+  if (loading) {
+    return (
+      <main>
+        <h1>Loading</h1>
+      </main>
+    );
+  }
 
   return (
     <main>
@@ -17,18 +37,27 @@ const Register = () => {
             name="email"
             id="email"
             placeholder="Enter email"
+            onInput={(e) => {
+              setEmail(e.target.value);
+            }}
           />
           <input
             type="text"
             name="username"
             id="username"
             placeholder="Enter username"
+            onInput={(e) => {
+              setUsername(e.target.value);
+            }}
           />
           <input
             type="text"
             name="password"
             id="password"
             placeholder="Enter password"
+            onInput={(e) => {
+              setPassword(e.target.value);
+            }}
           />
           <button className="button primary-button">Register</button>
         </form>
