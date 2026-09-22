793028f4845b6e3920c94d50378ce96bcb0d7a80
manaskg <manaskumarghosh91@gmail.com>
2026-09-22 21:02:48 +0530

    feat: implement song upload functionality with multer middleware and update routes

--- day25/Backend/src/controllers/song.controller.js
+++ day25/Backend/src/controllers/song.controller.js
@@ -1,2 +1,7 @@
-const songModel = require("../models/songs.model.js")
+const songModel = require("../models/songs.model.js");
 
+async function uploadSong(req, res) {
+  console.log(req.file);
+}
+
+module.exports = { uploadSong };
--- day25/Backend/src/routes/song.routes.js
+++ day25/Backend/src/routes/song.routes.js
@@ -1,13 +1,12 @@
 const express = require("express");
-
+const upload = require("../middlewares/upload.middleware.js");
 const router = express.Router();
-
-
+const songController = require("../controllers/song.controller.js");
 
 /**
  * Post /api/songs/
  */
 
-router.post("/",)
+router.post("/", upload.single("song"), songController.uploadSong);
 
 module.exports = router;
