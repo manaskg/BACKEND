38180d236300ead27523264196332f12066e4d9a
manaskg <manaskumarghosh91@gmail.com>
2026-09-23 22:55:49 +0530

    feat: optimize song upload by using Promise.all for concurrent file uploads

--- day25/Backend/src/controllers/song.controller.js
+++ day25/Backend/src/controllers/song.controller.js
@@ -11,17 +11,30 @@ async function uploadSong(req, res) {
 
   // console.log(tags);
 
-  const songFile = await storageService.uploadFile({
-    buffer: songBuffer,
-    filename: tags.title + ".mp3",
-    folder: "/cohort-2/moodify/songs",
-  });
+  const [songFile, posterFile] = await Promise.all([
+    storageService.uploadFile({
+      buffer: songBuffer,
+      filename: tags.title + ".mp3",
+      folder: "/cohort-2/moodify/songs",
+    }),
+    storageService.uploadFile({
+      buffer: tags.image.imageBuffer,
+      filename: tags.title + ".jpeg",
+      folder: "/cohort-2/moodify/posters",
+    }),
+  ]);
 
-  const posterFile = await storageService.uploadFile({
-    buffer: tags.image.imageBuffer,
-    filename: tags.title + ".jpeg",
-    folder: "/cohort-2/moodify/posters",
-  });
+  // const songFile = await storageService.uploadFile({
+  //   buffer: songBuffer,
+  //   filename: tags.title + ".mp3",
+  //   folder: "/cohort-2/moodify/songs",
+  // });
+
+  // const posterFile = await storageService.uploadFile({
+  //   buffer: tags.image.imageBuffer,
+  //   filename: tags.title + ".jpeg",
+  //   folder: "/cohort-2/moodify/posters",
+  // });
 
   const song = await songModel.create({
     title: tags.title,
